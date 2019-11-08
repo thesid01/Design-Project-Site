61e4fd1f71eaeb4414096b60b5bb061f3dc750fa
thesid01 <35187359+thesid01@users.noreply.github.com>
2019-11-09 02:20:18 +0530

    Update server.js

--- mongodb/server.js
+++ mongodb/server.js
@@ -1,6 +1,6 @@
 
 const mongoose=require('mongoose');  
-const url="mongodb://localhost:27017/productdb";  
+const url="mongodb://sid:sid123@ds149672.mlab.com:49672/casa";  
     
 mongoose.connect(url,{useNewUrlParser:true},(err)=>{  
     if(err){  
